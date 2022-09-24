84d3722b377abdf29aeb54acc5f455767ef22dfc
Marat Rafigullin <email.marat@gmail.com>
2022-09-24 20:22:52 +0300

    added Task2

---
+++ Task2.py
@@ -0,0 +1,8 @@
+# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            left = not (x or y or z)
+            right = not x and not y and not z
+            result = left == right
+            print(result)
